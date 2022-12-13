42ddab81a82ff0f1a45c768f6bc406a50091a850
Agrahari_ Yash (yasagrahari@deloitte.com) <yasagrahari@your.hostname.com>
2022-12-13 14:36:49 +0530

    chnaged and added election_id and candidate id

--- backend/election/views.py
+++ backend/election/views.py
@@ -229,29 +229,28 @@ class VerifyAndGetElectionsDetailsViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, url_path=r'get-electionwise-candidates',)
     def get_electionwise_candidates(self,request):
-        if self.request.user.is_authenticated:
-            user = self.request.query_params.get('aadhar_no')
-            if 'session_id' in self.request.query_params: 
-                session_id = self.request.query_params.get('session_id')
-            else:
-                session = ElectionTimingHelper.get_current_session()
-                session_id = session.uid
-            user_profile = UserProfile.objects.get(aadhar=user)
-            eligible_voter = EligibleVoters_2022.objects.filter(uniq_id=user_profile.uniq_id).values()
-            if(len(eligible_voter)>0 and eligible_voter[0]['is_voted'] == False):
-                phase_ids = ElectionStateWiseConsituency_2022.objects.filter(constituency=eligible_voter[0]['constituency'],session=session).values_list('phase_stateid')
-                elections = ElectionPhaseWiseState_2022.objects.filter(uid__in=phase_ids).values()
-                data = []
-                for election in elections:
-                    election_details = ElectionInfo.objects.filter(id=election['election_id_id']).values()
-                    # check_lock = ElectionLockingUnlocking_2022.objects.filter(starttime__gte=datetime.now(), endtime__lte=datetime.now(), phase=election['phase'],election_id=election['election_id_id']).exists()
-                    if(True):
-                        constiuency_id = ElectionStateWiseConsituency_2022.objects.filter(phase_stateid=election['uid'],constituency = eligible_voter[0]['constituency']).values('uid')
-                        candidates = list(ElectionCandidates_2022.objects.filter(constituency=constiuency_id[0]['uid']).values('candidate_id__first_name','candidate_id__last_name','constituency_id','party_id'))
-                        obj = {
-                            election_details[0]['title'] : candidates
-                        }
-                        data.append(obj)
-                return Response(data)
-            else:
-                return Response({"msg":"You are not allowed to vote"},status=204)
+        user = self.request.query_params.get('aadhar_no')
+        if 'session_id' in self.request.query_params: 
+            session_id = self.request.query_params.get('session_id')
+        else:
+            session = ElectionTimingHelper.get_current_session()
+            session_id = session.uid
+        user_profile = UserProfile.objects.get(aadhar=user)
+        eligible_voter = EligibleVoters_2022.objects.filter(uniq_id=user_profile.uniq_id).values()
+        if(len(eligible_voter)>0 and eligible_voter[0]['is_voted'] == False):
+            phase_ids = ElectionStateWiseConsituency_2022.objects.filter(constituency=eligible_voter[0]['constituency'],session=session).values_list('phase_stateid')
+            elections = ElectionPhaseWiseState_2022.objects.filter(uid__in=phase_ids).values()
+            data = []
+            for election in elections:
+                election_details = ElectionInfo.objects.filter(id=election['election_id_id']).values()
+                # check_lock = ElectionLockingUnlocking_2022.objects.filter(starttime__gte=datetime.now(), endtime__lte=datetime.now(), phase=election['phase'],election_id=election['election_id_id']).exists()
+                if(True):
+                    constiuency_id = ElectionStateWiseConsituency_2022.objects.filter(phase_stateid=election['uid'],constituency = eligible_voter[0]['constituency']).values('uid')
+                    candidates = list(ElectionCandidates_2022.objects.filter(constituency=constiuency_id[0]['uid']).values('candidate_id__first_name','candidate_id__last_name','constituency_id','party_id','candidate_id','constituency_id__phase_stateid__election_id'))
+                    obj = {
+                        election_details[0]['title'] : candidates,
+                    }
+                    data.append(obj)
+            return Response(data)
+        else:
+            return Response({"msg":"You are not allowed to vote"},status=204)
